ba67d12a019989f1020fc29c7f561bc2308b4473
AndresBena19 <andres_felipe16@hotmail.es>
2018-03-14 19:17:33 -0500

    sadsad
    das

--- Inventory/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config(object):
-    SECRET_KEY= 'KEY'
-
-class DBconfig(Config):
-    DEBUG =True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database/DB.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
